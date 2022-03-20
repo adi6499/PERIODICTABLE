07882f34531520337ad2e5be8cc0971bc9b6352e
adi6499 <83864698+adi6499@users.noreply.github.com>
2022-03-20 08:28:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ let box = document.querySelectorAll(".box")
 box.forEach((b)=>{
     b.addEventListener("click",(e)=>{
      let voice = new SpeechSynthesisUtterance(e.target.textContent)
+        window.speechSynthesis.cancel();
      speechSynthesis.speak(voice)
-    voice.currentTime=0;
+
        
     })
 })
